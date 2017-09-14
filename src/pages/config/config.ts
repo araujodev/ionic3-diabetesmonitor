b26392230aeace734f9ea0fe392db08392fe0cce
System Administrator <root@MacBook-Pro.local>
2017-09-14 09:50:37 -0400

    Ajustes e configuracoes iniciais - tela black sendo feita

--- src/pages/config/config.ts
+++ src/pages/config/config.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+  selector: 'page-config',
+  templateUrl: 'config.html'
 })
-export class AboutPage {
+export class ConfigPage {
 
   constructor(public navCtrl: NavController) {
 
